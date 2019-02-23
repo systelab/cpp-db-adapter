0d212929459c46a9fadadaa8446d5edf440b6de0
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-02-23 11:24:19 +0100

    Restored db comparator from snow

--- DbAdapterTestUtilities/Comparators/DbComparator.h
+++ DbAdapterTestUtilities/Comparators/DbComparator.h
@@ -1,144 +1,153 @@
-#ifndef CPPSQLITEDBADAPTER_TEST_UTILITIES_COMPARATORS_DBCOMPARATOR_H_
-#define CPPSQLITEDBADAPTER_TEST_UTILITIES_COMPARATORS_DBCOMPARATOR_H_
+#ifndef _DBADAPTERTESTUTILITIES_DBCOMPARATOR_DMC_0410161948_H
+#define _DBADAPTERTESTUTILITIES_DBCOMPARATOR_DMC_0410161948_H
 
-#include "../Stubs/StubField.h"
-#include "../Stubs/StubFieldValue.h"
-#include "../Stubs/StubTableRecord.h"
-#include <gtest/gtest.h>
+#include "TestUtilities/DbAdapter/Stubs/StubField.h"
+#include "TestUtilities/DbAdapter/Stubs/StubFieldValue.h"
+#include "TestUtilities/DbAdapter/Stubs/StubTableRecord.h"
+
 
 using namespace testing;
 
-namespace systelab {
-namespace test_utility {
+namespace systelab { namespace test_utility {
 
-inline testing::AssertionResult DbComparator(const db::IField &expected,
-                                             const db::IField &actual) {
-  if (typeid(expected) != typeid(actual)) {
-    return AssertionFailure() << "Different class type: "
-                              << "expected=" << typeid(expected).name()
-                              << ", actual=" << typeid(actual).name();
-  }
+	inline testing::AssertionResult DbComparator(const db::IField& expected, const db::IField& actual)
+	{
+		if (typeid(expected) != typeid(actual))
+		{
+			return AssertionFailure() << "Different class type: "
+										<< "expected=" << typeid(expected).name()
+										<< ", actual=" << typeid(actual).name();
+		}
 
-  if (expected.getName() != actual.getName()) {
-    return AssertionFailure() << "Different Name value: "
-                              << "expected=" << expected.getName()
-                              << ", actual=" << actual.getName();
-  }
+		if (expected.getName() != actual.getName())
+		{
+			return AssertionFailure() << "Different Name value: "
+										<< "expected=" << expected.getName()
+										<< ", actual=" << actual.getName();
+		}
 
-  if (expected.getType() != actual.getType()) {
-    return AssertionFailure() << "Different Type value: "
-                              << "expected=" << expected.getType()
-                              << ", actual=" << actual.getType();
-  }
+		if (expected.getType() != actual.getType())
+		{
+			return AssertionFailure() << "Different Type value: "
+										<< "expected=" << expected.getType()
+										<< ", actual=" << actual.getType();
+		}
 
-  return AssertionSuccess();
-}
+		return AssertionSuccess();
+	}
 
-inline testing::AssertionResult DbComparator(const db::IFieldValue &expected,
-                                             const db::IFieldValue &actual) {
-  if (typeid(expected) != typeid(actual)) {
-    return AssertionFailure() << "Different class type: "
-                              << "expected=" << typeid(expected).name()
-                              << ", actual=" << typeid(actual).name();
-  }
+	inline testing::AssertionResult DbComparator(const db::IFieldValue& expected, const db::IFieldValue& actual)
+	{
+		if (typeid(expected) != typeid(actual))
+		{
+			return AssertionFailure() << "Different class type: "
+										<< "expected=" << typeid(expected).name()
+										<< ", actual=" << typeid(actual).name();
+		}
 
-  if (expected.isDefault() != actual.isDefault()) {
-    return AssertionFailure() << "Different Is Default value: "
-                              << "expected=" << expected.isDefault()
-                              << ", actual=" << actual.isDefault();
-  }
+		if (expected.isDefault() != actual.isDefault())
+		{
+			return AssertionFailure() << "Different Is Default value: "
+										<< "expected=" << expected.isDefault()
+										<< ", actual=" << actual.isDefault();
+		}
 
-  if (expected.isNull() != actual.isNull()) {
-    return AssertionFailure() << "Different Is Null value: "
-                              << "expected=" << expected.isNull()
-                              << ", actual=" << actual.isNull();
-  }
+		if (expected.isNull() != actual.isNull())
+		{
+			return AssertionFailure() << "Different Is Null value: "
+										<< "expected=" << expected.isNull()
+										<< ", actual=" << actual.isNull();
+		}			
+			
+		if (!expected.isNull())
+		{
+			systelab::db::FieldTypes fieldType = expected.getField().getType();
 
-  if (!expected.isNull()) {
-    systelab::db::FieldTypes fieldType = expected.getField().getType();
+			if (fieldType == systelab::db::BOOLEAN && (expected.getBooleanValue() != actual.getBooleanValue()))
+			{
+				return AssertionFailure() << "Different Boolean value: "
+										<< "expected=" << expected.getBooleanValue()
+										<< ", actual=" << actual.getBooleanValue();
+			}
+				
+			if (fieldType == systelab::db::INT && (expected.getIntValue() != actual.getIntValue()))
+			{
+				return AssertionFailure() << "Different Integer value: "
+										<< "expected=" << expected.getIntValue()
+										<< ", actual=" << actual.getIntValue();
+			}
 
-    if (fieldType == systelab::db::BOOLEAN &&
-        (expected.getBooleanValue() != actual.getBooleanValue())) {
-      return AssertionFailure() << "Different Boolean value: "
-                                << "expected=" << expected.getBooleanValue()
-                                << ", actual=" << actual.getBooleanValue();
-    }
+			if (fieldType == systelab::db::DOUBLE && (expected.getDoubleValue() != actual.getDoubleValue()))
+			{
+				return AssertionFailure() << "Different Double value: "
+										<< "expected=" << expected.getDoubleValue()
+										<< ", actual=" << actual.getDoubleValue();
+			}
 
-    if (fieldType == systelab::db::INT &&
-        (expected.getIntValue() != actual.getIntValue())) {
-      return AssertionFailure() << "Different Integer value: "
-                                << "expected=" << expected.getIntValue()
-                                << ", actual=" << actual.getIntValue();
-    }
+			if (fieldType == systelab::db::STRING && (expected.getStringValue() != actual.getStringValue()))
+			{
+				return AssertionFailure() << "Different String value: "
+										<< "expected=" << expected.getStringValue()
+										<< ", actual=" << actual.getStringValue();
+			}
 
-    if (fieldType == systelab::db::DOUBLE &&
-        (expected.getDoubleValue() != actual.getDoubleValue())) {
-      return AssertionFailure() << "Different Double value: "
-                                << "expected=" << expected.getDoubleValue()
-                                << ", actual=" << actual.getDoubleValue();
-    }
+			if (fieldType == systelab::db::DATETIME && (expected.getDateTimeValue() != actual.getDateTimeValue()))
+			{
+				return AssertionFailure() << "Different DateTime value: "
+										<< "expected=" << expected.getDateTimeValue()
+										<< ", actual=" << actual.getDateTimeValue();
+			}
+		}
 
-    if (fieldType == systelab::db::STRING &&
-        (expected.getStringValue() != actual.getStringValue())) {
-      return AssertionFailure() << "Different String value: "
-                                << "expected=" << expected.getStringValue()
-                                << ", actual=" << actual.getStringValue();
-    }
+		const db::IField& expectedStepField = expected.getField();
+		const db::IField& actualStepField = actual.getField();
+		
+		AssertionResult stubFieldsComparisonResult = DbComparator(expectedStepField, actualStepField);
+		if (!stubFieldsComparisonResult)
+		{
+			return AssertionFailure() << stubFieldsComparisonResult.message();
+		}
 
-    if (fieldType == systelab::db::DATETIME &&
-        (expected.getDateTimeValue() != actual.getDateTimeValue())) {
-      return AssertionFailure() << "Different DateTime value: "
-                                << "expected=" << expected.getDateTimeValue()
-                                << ", actual=" << actual.getDateTimeValue();
-    }
-  }
+		return AssertionSuccess();
+	}
 
-  const db::IField &expectedStepField = expected.getField();
-  const db::IField &actualStepField = actual.getField();
+	inline testing::AssertionResult DbComparator(const db::ITableRecord& expected, const db::ITableRecord& actual)
+	{
+		if (typeid(expected) != typeid(actual))
+		{
+			return AssertionFailure() << "Different class type: "
+									  << "expected=" << typeid(expected).name()
+									  << ", actual=" << typeid(actual).name();
+		}
 
-  AssertionResult stubFieldsComparisonResult =
-      DbComparator(expectedStepField, actualStepField);
-  if (!stubFieldsComparisonResult) {
-    return AssertionFailure() << stubFieldsComparisonResult.message();
-  }
+		unsigned int nExpectedFieldValues = expected.getFieldValuesCount();
+		unsigned int nActualFieldValues = actual.getFieldValuesCount();
+		if (nExpectedFieldValues != nActualFieldValues)
+		{
+			return AssertionFailure() << "Different Number of field values: "
+									  << "expected=" << nExpectedFieldValues
+									  << ", actual=" << nActualFieldValues;
+		}
 
-  return AssertionSuccess();
-}
+		for (unsigned int i = 0; i < nExpectedFieldValues; i++)
+		{
+			const db::IFieldValue& fieldValue1 = expected.getFieldValue(i);
+			const db::IFieldValue& fieldValue2 = actual.getFieldValue(i);
+		
+			AssertionResult stubFieldsComparisonResult = DbComparator(fieldValue1, fieldValue2);
+			if (!stubFieldsComparisonResult)
+			{
+				return AssertionFailure() << "Different data for field " << i << " "
+										  << "(name='" + fieldValue1.getField().getName() + "'): "
+										  << stubFieldsComparisonResult.message();
+			}
+		}
 
-inline testing::AssertionResult DbComparator(const db::ITableRecord &expected,
-                                             const db::ITableRecord &actual) {
-  if (typeid(expected) != typeid(actual)) {
-    return AssertionFailure() << "Different class type: "
-                              << "expected=" << typeid(expected).name()
-                              << ", actual=" << typeid(actual).name();
-  }
+		return AssertionSuccess();
+	}
 
-  unsigned int nExpectedFieldValues = expected.getFieldValuesCount();
-  unsigned int nActualFieldValues = actual.getFieldValuesCount();
-  if (nExpectedFieldValues != nActualFieldValues) {
-    return AssertionFailure() << "Different Number of field values: "
-                              << "expected=" << nExpectedFieldValues
-                              << ", actual=" << nActualFieldValues;
-  }
 
-  for (unsigned int i = 0; i < nExpectedFieldValues; i++) {
-    const db::IFieldValue &fieldValue1 = expected.getFieldValue(i);
-    const db::IFieldValue &fieldValue2 = actual.getFieldValue(i);
 
-    AssertionResult stubFieldsComparisonResult =
-        DbComparator(fieldValue1, fieldValue2);
-    if (!stubFieldsComparisonResult) {
-      return AssertionFailure()
-             << "Different data for field " << i << " "
-             << "(name='" + fieldValue1.getField().getName() + "'): "
-             << stubFieldsComparisonResult.message();
-    }
-  }
+}};
 
-  return AssertionSuccess();
-}
-
-} // namespace test_utility
-}; // namespace systelab
-
-#endif // CPP_SQLITE_DB_ADAPTER_TEST_UTILITIES_COMPARATORS_DBCOMPARATOR_H_
+#endif //_DBADAPTERTESTUTILITIES_DBCOMPARATOR_DMC_0410161948_H
