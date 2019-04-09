ea2b5d4654ea3464e6c5c1e70b972758281cf171
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-04-09 12:57:06 +0200

    Declared connection exceptions

--- DbAdapterInterface/IConnection.h
+++ DbAdapterInterface/IConnection.h
@@ -5,13 +5,25 @@
 
 #include <memory>
 
+
 namespace systelab { namespace db {
+
 	class IConnection
 	{
 	public:
 		virtual ~IConnection() {}
 
 		virtual std::unique_ptr<IDatabase> loadDatabase(IConnectionConfiguration&) = 0;
+
+	public:
+		struct Exception : public std::runtime_error
+		{
+			Exception(const std::string& message)
+				: std::runtime_error(message)
+			{
+			}
+		};
 	};
+
 }}
 
