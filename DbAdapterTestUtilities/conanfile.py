7cf9a90a90c5035aab329ccf6e92f8b305c9e4bb
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-12-09 19:57:24 +0100

    Added boost as a requirement

--- DbAdapterTestUtilities/conanfile.py
+++ DbAdapterTestUtilities/conanfile.py
@@ -25,6 +25,7 @@ class DbAdapterTestUtilitiesConan(ConanFile):
         else:
             self.requires("gtest/1.8.1@bincrafters/stable")
 
+        self.requires(("boost/%s@conan/stable") % self.options.boost)
         self.requires("TestUtilitiesInterface/1.0.4@systelab/stable")
         if ("%s" % self.version) == "None":
             self.requires("DbAdapterInterface/%s@systelab/stable" % os.environ['VERSION'])
