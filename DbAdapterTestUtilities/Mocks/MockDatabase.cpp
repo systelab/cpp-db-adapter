31e36186bd2f667cb5c5cb1a6a249e79bd483c7b
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-12-09 15:17:57 +0100

    Removed compilation warning

--- DbAdapterTestUtilities/Mocks/MockDatabase.cpp
+++ DbAdapterTestUtilities/Mocks/MockDatabase.cpp
@@ -10,8 +10,8 @@ namespace systelab { namespace db { namespace test_utility {
 
 	MockDatabase::~MockDatabase()
 	{
-		unsigned int nTables = m_tables.size();
-		for (unsigned int i = 0; i < nTables; i++)
+		size_t nTables = m_tables.size();
+		for (size_t i = 0; i < nTables; i++)
 		{
 			delete m_tables[i];
 		}
